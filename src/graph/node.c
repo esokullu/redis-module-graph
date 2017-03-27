b3b9ff8b54607b7b1c53e8a0b49372e26c2ab6d0
Emre Sokullu <esokullu@gmail.com>
2017-03-27 04:47:38 +0300

    Update node.c

--- src/graph/node.c
+++ src/graph/node.c
@@ -8,7 +8,7 @@ Node* NewNode(const char* alias, const char* id) {
 	node->alias = NULL;
 	node->outgoingEdges = NewVector(Edge*, 0);
 	node->incomingEdges = 0;
-	node->internalId = randstring();
+	node->internalId = &randstring();
 
 	if(id != NULL) {
 		node->id = strdup(id);
